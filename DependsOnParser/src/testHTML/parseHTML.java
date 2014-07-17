57c8096c688d60f2be7adbfae600f6ef4ee248c0
Jason Weaver <jason.weaver@utexas.edu>
2014-07-16 23:25:04 -0500

    Algorithm flaw.  Rewrote to be recursion. Output more realistic. Runtime about 3 minutes.

--- DependsOnParser/src/testHTML/parseHTML.java
+++ DependsOnParser/src/testHTML/parseHTML.java
@@ -4,13 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 
 public class parseHTML {
 
@@ -24,54 +23,59 @@ public class parseHTML {
 		} catch (IOException e) { 
 			e.printStackTrace(); }
 		
-		List<String> dependants = Arrays.asList(	// initial three classes used
+		Set<String> dependants = new HashSet<String>(Arrays.asList(		// initial three classes used
 			"org.apache.poi.xwpf.usermodel.XWPFDocument",
 			"org.apache.poi.xwpf.usermodel.XWPFParagraph",
-			"org.apache.poi.xwpf.usermodel.XWPFRun");
+			"org.apache.poi.xwpf.usermodel.XWPFRun"));
 		
-		Set<String> supporters = traverseHTML(doc, dependants);		// get all the classes that the dependants need
-		supporters.addAll(dependants);							// set initial classes in total classes needed too
+		Set<String> supporters = findAllClasses(doc, dependants, dependants);		// get all the classes that the dependents need
 		
-		for (String supporter : supporters) System.out.println(supporter);
+		for (String supporter : supporters) System.out.println(supporter);			// show results
 	}
 	
 	
-	public static Set<String> traverseHTML(Document doc, List<String> list) {
-		int dpendantsSize = list.size();
+	// If there isn't a matching element, that means that class doesn't have a dependency
+	public static Set<String> findAllClasses(Document doc, Set<String> totalClasses, Set<String> currentDependants) {
+		Set<String> supporters = new HashSet<String>();
 		
-		if (dpendantsSize == 0) {		// Stopping condition
-			Set<String> supporters = new HashSet<String>();
-			return supporters;
-		}
-		
-		if (dpendantsSize == 1) {		// Stopping condition
+		for (String dependantString: currentDependants) {
 			Element dependant = null;
 			Elements foundElements = null, matchingElements = null;
+			String regex = "^" + dependantString + "$";
 			
-			try {				// create corresponding element/dependent
-				foundElements = doc.getElementsContainingOwnText(list.get(0));	// has correct text			
-				matchingElements = foundElements.select("td:eq(0)");							// has correct position
+			try {																		
+				foundElements = doc.getElementsMatchingOwnText(regex);		// has correct text			
+				matchingElements = foundElements.select("td:eq(0)");					// has correct position
+				
+				if (matchingElements.size() == 0) continue;								// no supporters
+				else if (matchingElements.size() == 1) ;
+				else throw new NumberFormatException();							// Check uniqueness
+				
 				dependant = matchingElements.first();
 				
-				if (matchingElements.size() != 1) throw new NumberFormatException();				// Check uniqueness
-			} catch (Exception e) {
-				for (Element matchingElement : matchingElements) System.out.println(matchingElement.text());
-				e.printStackTrace(); }
+			} catch (NullPointerException e) {														
+				continue; 
+			} catch (Exception e) {	
+				System.out.println("Regular Expression is: " + regex);
+				System.out.println("Elements are: ");
+				for (Element element: matchingElements) System.out.println(element.text());	
+				
+				e.printStackTrace(); 
+			}															
 			
 			String supportersString = dependant.nextElementSibling().ownText();		// update total classes needed
-			Set<String> supporters = new HashSet<String>(Arrays.asList(supportersString.split("\\s*,\\s*")));		
+			supporters.addAll(Arrays.asList(supportersString.split("\\s*,\\s*")));
+		  }
 			
-			return supporters;
-		}
+		Set<String> untestedSupporters = supporters;
+		untestedSupporters.removeAll(totalClasses);				// only check ones that haven't been yet
 		
-		// Divide
-		Set<String> supporters1 = traverseHTML(doc, list.subList(0, (dpendantsSize/2)-1));
-		Set<String> supporters2 = traverseHTML(doc, list.subList(dpendantsSize/2, dpendantsSize));
+		if (untestedSupporters.size() == 0) return totalClasses;		// no further supporters possible!!!
+
+		totalClasses.addAll(supporters);						// add found supporters/classes to total classes
 		
-		// Conquer
-		supporters1.addAll(supporters2);
-		
-		return supporters1;
+		// Recursion: check if supports are also dependents
+		return findAllClasses(doc, totalClasses, supporters); 	
 	}
 
 }
